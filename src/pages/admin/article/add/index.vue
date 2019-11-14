51ce2e339d87d2dfb89800e86ed23c5887dba25d
chenggang <1129137164@qq.com>
2019-11-14 22:12:21 +0800

    添加文章校验添加

--- src/pages/admin/article/add/index.vue
+++ src/pages/admin/article/add/index.vue
@@ -17,59 +17,9 @@
         <input v-model="form.title" type="text" class="form-control" id="title" placeholder="请输入内容标题" name="title">
       </div>
       <div class="form-group">
-        <label for="author">作者：</label>
-        <input v-model="form.author" type="text" class="form-control" id="author" placeholder="请输入作者姓名" name="author">
-      </div>
-      <div class="form-group">
-        <label for="keyword">关键字：</label>
-        <input v-model="form.keyword" type="text" class="form-control" id="keyword" placeholder="请输入关键字" name="keyword">
-      </div>
-      <div class="form-group">
         <label for="description">简介：</label>
         <input v-model="form.desc" type="text" class="form-control" id="description" placeholder="请输入内容简介"
                name="description">
-      </div>
-      <div class="form-group">
-        <label for="img_url">图片链接：</label>
-        <input v-model="form.img_url" type="text" class="form-control" id="img_url" placeholder="请输入图片链接"
-               name="img_url">
-      </div>
-      <div class="form-group">
-        <label for="tags">标签：</label>
-        <input v-model="form.tags" type="text" class="form-control" id="tags" placeholder="请输入标签" name="tags">
-      </div>
-      <div class="form-group">
-        <label>文章类型：</label>
-        <label class="radio-inline">
-          <input v-model="form.type" type="radio" name="type" value="0"> 普通文章
-        </label>
-        <label class="radio-inline">
-          <input v-model="form.type" type="radio" name="type" value="1"> 简历
-        </label>
-        <label class="radio-inline">
-          <input v-model="form.type" type="radio" name="type" value="2"> 管理员
-        </label>
-      </div>
-      <div class="form-group">
-        <label>文章发布状态：</label>
-        <label class="radio-inline">
-          <input v-model="form.state" type="radio" name="state" value="0"> 草稿
-        </label>
-        <label class="radio-inline">
-          <input v-model="form.state" type="radio" name="state" value="1"> 已发布
-        </label>
-      </div>
-      <div class="form-group">
-        <label>文章转载状态：</label>
-        <label class="radio-inline">
-          <input v-model="form.origin" type="radio" name="origin" value="0"> 原创
-        </label>
-        <label class="radio-inline">
-          <input v-model="form.origin" type="radio" name="origin" value="1"> 转载
-        </label>
-        <label class="radio-inline">
-          <input v-model="form.origin" type="radio" name="origin" value="2"> 混合
-        </label>
       </div>
       <div class="form-group">
         <label>文章内容：</label>
@@ -113,6 +63,10 @@
     },
     methods: {
       handleClick() {
+        if(!this.form.category) return toast({position:'top', message: '分类名称不能为空！'})
+        if(!this.form.title) return toast({position:'top', message: '标题不能为空！'})
+        if(!this.form.desc) return toast({position:'top', message: '简介不能为空！'})
+        if(!this.form.content) return toast({position:'top', message: '内容不能为空！'})
         this.$api.addArticle(this.form).then(res => {
           const data = res.data
           toast({
--- src/pages/admin/category/edit/index.vue
+++ src/pages/admin/category/edit/index.vue
@@ -1,21 +1,19 @@
 <template>
-  <div>
-    <ol class="breadcrumb">
-      <li><a href="/">首页</a></li>
-      <li><span>分类添加</span></li>
-    </ol>
-    <h3>分类添加</h3>
-    <form method="post">
-      <div class="form-group">
-        <label for="name">分类名称：</label>
-        <input v-model="name" type="text" class="form-control" id="name" placeholder="请输入分类名称" name="name">
-      </div>
-      <div class="form-group">
-        <label for="desc">分类介绍：</label>
-        <input v-model="desc" type="text" class="form-control" id="desc" placeholder="请输入分类介绍" name="desc">
-      </div>
-      <button type="button" class="btn btn-default" @click="add">提交</button>
-    </form>
+  <div class="row">
+    <div class="col-lg-offset-0 col-md-11">
+      <ol class="breadcrumb">
+        <li><a href="/">首页</a></li>
+        <li><span>分类添加</span></li>
+      </ol>
+      <h3>分类添加</h3>
+      <form method="post">
+        <div class="form-group">
+          <label for="name">分类名称：</label>
+          <input v-model="name" type="text" class="form-control" id="name" placeholder="请输入分类名称" name="name">
+        </div>
+        <button type="button" class="btn btn-default" @click="add">提交</button>
+      </form>
+    </div>
   </div>
 </template>
 
@@ -26,8 +24,7 @@
     name: "category-edit",
     data() {
       return {
-        name: '',
-        desc: ''
+        name: ''
       }
     },
     created() {
@@ -35,22 +32,20 @@
       this.$api.categoryDetail({id}).then(res => {
         const data = res.data
         this.name = data.data.name
-        this.desc = data.data.desc
       })
     },
     methods: {
       add() {
-        if (!this.name || !this.desc) {
+        if (!this.name) {
           toast({
             position: 'top',
-            message: '分类名称或介绍不能为空！'
+            message: '分类名称不能为空！'
           })
           return false
         }
         const data = {
           id: this.$route.params.id,
-          name: this.name,
-          desc: this.desc
+          name: this.name
         }
         this.$api.updateCategory(data).then(res => {
           const data = res.data
